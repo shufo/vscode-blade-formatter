83750c9a85ed29bd11bfd2f194d00ba097d0e599
shufo <meikyowise@gmail.com>
2022-06-16 07:25:47 +0900

    feat: 🎸 config `dontShowNewVersion` to suppress update message

--- src/extension.ts
+++ src/extension.ts
@@ -155,6 +155,12 @@ function shouldIgnore(filepath: any) {
 }
 
 function showWelcomeMessage(context: vscode.ExtensionContext) {
+    const extConfig: WorkspaceConfiguration = vscode.workspace.getConfiguration("bladeFormatter.misc");
+
+    if (extConfig.dontShowNewVersionMessage) {
+        return;
+    }
+
     let message: string | null = null;
 
     const previousVersion = context.globalState.get<string>(ExtensionConstants.globalVersionKey);
