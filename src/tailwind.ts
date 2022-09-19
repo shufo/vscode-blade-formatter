9fb09fd352d121346fe44662634d4d4fe6fbec75
shufo <meikyowise@gmail.com>
2022-09-19 12:24:05 +0900

    feat: 🎸 add runtime option `tailwindcssConfigPath`

---
+++ src/tailwind.ts
@@ -0,0 +1,25 @@
+import findConfig from 'find-config';
+import path from 'path';
+import { findConfigFile } from './runtimeConfig';
+
+const __config__ = 'tailwind.config.js';
+
+/**
+ * Resolve tailwind config path if resolvable
+ *
+ * @param filepath string
+ * @param optionPath string
+ */
+export function resolveTailwindConfig(filepath: string, optionPath: string): string {
+    if (!optionPath) {
+        return findConfig(__config__, { cwd: path.dirname(filepath) }) ?? '';
+    }
+
+    if (path.isAbsolute(optionPath ?? '')) {
+        return optionPath;
+    }
+
+    const runtimeConfigPath = findConfigFile(filepath);
+
+    return path.resolve(path.dirname(runtimeConfigPath ?? ''), optionPath ?? '');
+}
