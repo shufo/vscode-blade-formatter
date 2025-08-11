e0d34bbc887e73b311d79a33eb85eae063583129
Shuhei Hayashibara <meikyowise@gmail.com>
2025-08-11 15:56:47 +0900

    test: 💍 use github action specified temp dir

--- src/test/runE2ETest.ts
+++ src/test/runE2ETest.ts
@@ -7,105 +7,114 @@ import path, { dirname } from "path";
 import { downloadAndUnzipVSCode, runTests } from "@vscode/test-electron";
 
 const VSCODE_VERSION = process.env.VSCODE_VERSION;
+const TMP_DIR = process.env.TMP_DIR;
 
 async function main() {
-	try {
-		// vscode version
-		const version = VSCODE_VERSION;
-		const codePath = await downloadAndUnzipVSCode(version);
+  try {
+    // vscode version
+    const version = VSCODE_VERSION;
+    const codePath = await downloadAndUnzipVSCode(version);
 
-		console.log(`Using VS Code at ${codePath}`);
+    console.log(`Using VS Code at ${codePath}`);
 
-		const codeExecPath = path.resolve(codePath, "../", "bin", "code");
+    const codeExecPath = path.resolve(codePath, "../", "bin", "code");
 
-		const extensionsDir = getExtensionsDir();
+    const tmpDir = TMP_DIR || tmpdir();
 
-		const extensionsDirArgs = ["--extensions-dir", extensionsDir];
-		// The path to the extension test script
-		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.resolve(__dirname, "./suite/index");
+    const extensionsDir = getExtensionsDir(tmpDir);
 
-		const packagePath = makePackagePath();
+    const extensionsDirArgs = ["--extensions-dir", extensionsDir];
+    // The path to the extension test script
+    // Passed to --extensionTestsPath
+    const extensionTestsPath = path.resolve(__dirname, "./suite/index");
 
-		// create .vsix package
-		await createPackage(packagePath);
+    const packagePath = makePackagePath(tmpDir);
 
-		await installExtension(codeExecPath, packagePath, extensionsDirArgs);
+    // create .vsix package
+    await createPackage(packagePath);
 
-		const launchArgs = [
-			`${path.resolve(
-				__dirname,
-				"../../src",
-			)}/test/fixtures/test.code-workspace`,
-			...extensionsDirArgs,
-		];
+    await installExtension(codeExecPath, packagePath, extensionsDirArgs);
 
-		// Download VS Code, unzip it and run the integration test
-		await runTests({
-			version,
-			extensionDevelopmentPath: "./support", // set directory that has no package.json
-			extensionTestsPath,
-			launchArgs,
-		});
-	} catch (err) {
-		console.error("Failed to run tests");
-		process.exit(1);
-	}
+    const launchArgs = [
+      `${path.resolve(
+        __dirname,
+        "../../src",
+      )}/test/fixtures/test.code-workspace`,
+      ...extensionsDirArgs,
+    ];
+
+    // Download VS Code, unzip it and run the integration test
+    await runTests({
+      version,
+      extensionDevelopmentPath: "./support", // set directory that has no package.json
+      extensionTestsPath,
+      launchArgs,
+    });
+  } catch (err) {
+    console.error("Failed to run tests");
+    process.exit(1);
+  }
 }
 
-function makePackagePath(): string {
-	const packageJsonPath = path.resolve(__dirname, "../../package.json");
-	const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
-	const packageName = packageJson.name;
-	const packageVersion = packageJson.version;
+function makePackagePath(tmpDir: string): string {
+  const packageJsonPath = path.resolve(__dirname, "../../package.json");
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+  const packageName = packageJson.name;
+  const packageVersion = packageJson.version;
 
-	const packagePath = path.resolve(
-		tmpdir(),
-		`${packageName}-${packageVersion}.vsix`,
-	);
+  const packagePath = path.resolve(
+    tmpDir,
+    `${packageName}-${packageVersion}.vsix`,
+  );
 
-	return packagePath;
+  return packagePath;
 }
 
 async function createPackage(packagePath: string) {
-	const result = spawnSync("yarn", ["run", "package", "--out", packagePath, "--allow-package-env-file"]);
+  const result = spawnSync("yarn", [
+    "run",
+    "package",
+    "--out",
+    packagePath,
+    "--allow-package-env-file",
+  ]);
 
-	if (result.status === 0) {
-		console.log("Package command succeeded");
-		console.log(result.stdout.toString());
-	} else {
-		console.error("Package command failed");
-		console.error(result.stderr.toString());
-	}
+  if (result.status === 0) {
+    console.log("Package command succeeded");
+    console.log(result.stdout.toString());
+  } else {
+    console.error("Package command failed");
+    console.error(result.stderr.toString());
+  }
 }
 
 async function installExtension(
-	executablePath: string,
-	packagePath: string,
-	args: string[],
+  executablePath: string,
+  packagePath: string,
+  args: string[],
 ) {
-	const result = spawnSync(executablePath, [
-		"--install-extension",
-		packagePath,
-		...args,
-	]);
+  const result = spawnSync(executablePath, [
+    "--install-extension",
+    packagePath,
+    ...args,
+  ]);
 
-	if (result.status === 0) {
-		console.log("install package succeeded");
-		console.log(result.stdout.toString());
-	} else {
-		console.error("install package failed");
-		console.error(result.stderr.toString());
-	}
+  if (result.status === 0) {
+    console.log("install package succeeded");
+    console.log(result.stdout.toString());
+  } else {
+    console.error("install package failed");
+    console.error(result.stderr.toString());
+  }
 }
 
-function getExtensionsDir(): string {
-	const randomBytesStr = randomBytes(4).toString("hex");
-	const extensionsDir = `${tmpdir()}/extensions_${randomBytesStr}`;
+function getExtensionsDir(tmpDir: string): string {
+  const randomBytesStr = randomBytes(4).toString("hex");
+  const extensionsDir = `${tmpDir}/extensions_${randomBytesStr}`;
 
-	console.log(`Using extensions dir: ${extensionsDir}`);
+  console.log(`Using extensions dir: ${extensionsDir}`);
 
-	return extensionsDir;
+  return extensionsDir;
 }
 
 main();
