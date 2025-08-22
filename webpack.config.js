d7797bf6cb3a9cfd8199e78d7ae015f6d0b9b07d
Isaac Emmanuel <35623334+1cbyc@users.noreply.github.com>
2025-08-22 09:17:19 +0100

    corrected the errors in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,119 +1,61 @@
-//@ts-check
+// @ts-check
 
-const path = require("node:path:path");
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const Dotenv = require("dotenv-webpack");
 
-const __nodeExternals = require("webpack-node-externa
-");
-const _Dotenv = require("dotenv-webpa
+/** @typedef {import('webpack').Configuration} WebpackConfig **/
 
-;
+/** @type WebpackConfig */
+const extensionConfig = {
+  target: "node", // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+  mode: "none", // leave source code close to original (set to 'production' when packaging)
+  entry: "./src/extension.ts", // entry point 📖 -> https://webpack.js.org/configuration/entry-context/
+  output: {
+    // bundle stored in 'dist' 📖 -> https://webpack.js.org/configuration/output/
+    path: path.resolve(__dirname, "dist"),
+    filename: "extension.js",
+    libraryTarget: "commonjs2",
+  },
+  externals: [
+    { vscode: "commonjs vscode" }, // vscode is provided by VS Code runtime
+    nodeExternals(),
+  ],
+  externalsPresets: {
+    node: true, // ignore built-in modules like path, fs, etc.
+  },
+  optimization: {
+    moduleIds: "named",
+  },
+  resolve: {
+    extensions: [".ts", ".js"], // support TypeScript and JavaScript
+  },
+  plugins: [
+    new Dotenv({
+      safe: false, // allow compilation without .env file
+      systemvars: true, // include system env vars
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true, // disable type checker, handled elsewhere
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devtool: "nosources-source-map",
+  infrastructureLogging: {
+    level: "log", // enables logging for problem matchers
+  },
+};
 
-/** @typedef {_Dotenv('webpack').Configuration} WebpackConfi
-
-/
-
-/** @type WebpackConf
- */
-const _extensionConfi
-= {
-	t_extensionConfi // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/
-de/
-	mode: "none", // this leaves the source code as close as possible to the original (when packaging we set this to 'product
-n')
-	entry: "./src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-con
-xt/
-	outp
-: {
-		// the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/ou
-ut/
-		path: path.resolve(__dirname, "di
-"),
-		filename: "extension
-s",
-		libraryTarget: "common
-2",
-	},
-	externa
-: [
-		{ vscode: "commonjs vscode" }, // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/exter
-ls/
-		// { "blade-formatter": "commonjs blade-formatter" }, // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/exter
-ls/
-		// modules added here also need to be added in the .vscodeignore
-ile
-		nodeExterna
-(),
-	],
-	externalsPrese
-: {
-		node: true, // in order to ignore built-in modules like path, fs,
-tc.
-	},
-	optimizati
-: {
-		moduleIds: "na
-d",
-	},
-	resol
-: {
-		// support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-l
-der
-		extensions: [".ts", ".
-"],
-	},
-	plugi
-: [
-		new Dot
-v({
-			safe: false, // Allow compilation without .env file for develo
-ent
-			systemvars: true, // Include system environment vari
-les
-
-}),
-	],
-	modu
-: {
-		rul
-: [
-		{
-				test: /\.
-$/,
-				exclude: /node_modu
-s/,
-				type: "javascript/a
-o",
-				u
-: [
-	
-		{
-						loader: "ts-loa
-r",
-						optio
-: {
-							// disable type checker - we will use it in fork p
-gin
-							transpileOnly: 
-ue,
-			
-	},
-		
-	},
-	
-	],
-
-	},
-	],
-	},
-	devtool: "nosources-source-
-p",
-	infrastructureLoggi
-: {
-		level: "log", // enables logging required for problem mat
-ers
-	}
-
-;
-
-module.exports = [extensionCon
-g];
+module.exports = [extensionConfig];
